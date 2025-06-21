459d157a3a61f5ea60db4c528d281370b796672a
ReTr010 <artem9mega@mail.ru>
2025-06-22 02:24:50 +0300

    Cookies, login, signup, casino, blackjack

--- Server_App/flask_server.py
+++ Server_App/flask_server.py
@@ -1,5 +1,9 @@
-from flask import Flask ,request, render_template
+from flask import Flask ,request, render_template, session, redirect, url_for
 import sqlite3
+from hashlib import sha256
+from contextlib import closing
+from random import randint
+import os
 
 # Подключение к базе данных SQLite для хранения пользователей
 conn = sqlite3.connect('bot_users.db')
@@ -10,38 +14,71 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL,
         password_hash TEXT NOT NULL, 
-        dataset_link TEXT,
+        dataset_link TEXT DEFAULT NULL,
         language TEXT DEFAULT 'ru'
     );
 ''')
 conn.commit()
+
+
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
 
 @app.route('/')
 def hello_world():
    return 'Hello World'
 
-@app.route("/registration", methods = ['POST', 'GET'])
-def registration():
-	if request.method == "GET":
-		return render_template("registration.html")
-	elif request.method == "POST":
-		pass
-	else:
-		return 'bad request!', 400
+@app.route("/signup", methods = ['POST', 'GET'])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        user_name = request.form.get("name")
+        user_email = request.form.get("email")
+        user_password = request.form.get("password")
+        user_password = sha256(user_password.encode('utf-8')).hexdigest()
+
+        with closing(sqlite3.connect('bot_users.db')) as conn:
+            with conn:
+                conn.execute('''
+                    INSERT INTO users (username,email, password_hash)
+                    VALUES (?, ?, ?)
+                    ''', (user_name, user_email, user_password))
+                user_id = conn.cursor().lastrowid
+                conn.commit()
+
+        session["user_id"] = user_id
+        session["user_name"] = user_name
+        return redirect("/")
+    else:
+        return 'bad request!', 400
 
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
    if request.method == "GET":
-      return render_template("login.html")
+        return render_template("login.html")
    elif request.method == "POST":
-      result = request.form.items()
-      for key,value in result:
-         print(key,value)
-      return render_template("login.html")
+        email_entered    = request.form.get(email)
+        password_entered = sha256(request.form.get(email).encode('utf-8')).hexdigest()
+        
+        with conn:
+                conn.execute('''
+                    INSERT INTO users (username,email, password_hash)
+                    VALUES (?, ?, ?)
+                    ''', (user_name, user_email, user_password))
+                user_id = conn.cursor().lastrowid
+                conn.commit()
+
+        return render_template("login.html")
    else:
-      return 'bad request!', 400
+        return 'bad request!', 400
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
+
+
+
+    <img src="{{}}">
